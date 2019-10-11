87612810396eecbdd229a50bc7cde377baeb6d23
Sqflex <32853595+Sqflex@users.noreply.github.com>
2019-10-11 16:53:43 +0300

    Add: Solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.quess=0;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
+        if(min==max){
+            console.log("Equal numbers!")
+        }
     }
 
     guess() {
-
+        this.quess = this.min + Math.round((this.max - this.min)/2);
+        return this.quess;
     }
 
     lower() {
-
-    }
-
-    greater() {
-
-    }
+        if (this.guess==this.min) {
+          throw new Error("Cheating! The Number is lower than the range!");
+        }
+        this.max = this.guess;
+      }
+  
+      greater() {
+        if (this._uess==this.min) {
+          throw new Error("Cheating! The Number is greater than the range!");
+        }
+        this.min = this.guess;
+      }
 }
 
 module.exports = GuessingGame;
